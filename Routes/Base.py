a9d21ead5b4b9beca30532ecf88f30f68fd7b456
pc <pc@DESKTOP-2N68CP9>
2026-04-11 09:08:58 +0200

    Test : adding test endpoints

---
+++ Routes/Base.py
@@ -0,0 +1,13 @@
+import os
+
+from fastapi import FastAPI , APIRouter , status , HTTPException
+# from dotenv import load_dotenv
+
+router = APIRouter(prefix="/api/v1", tags=["Base"])
+
+
+@router.get("/Welcome")
+async def WelcomeMessage():
+    app_name = os.getenv('APP-NAME')
+    app_version = os.getenv('APP-VERSION')
+    return f"Welcome to {app_name} v{app_version}"
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+from dotenv import load_dotenv
+load_dotenv(".env")
 from fastapi import FastAPI
+from Routes.Base import router as Base_Router
+
 
 app = FastAPI()
+app.include_router(Base_Router)
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# async def read_root():
+#     return {"Hello": "World"}   
 
-@app.get("/welcome")
-async def WelcomeMessage():
-    return "Welcome on board"
+# @app.get("/")
+# async def WelcomeMessage():
+#     return "Welcome on board"
 
-@app.get("/items/{itemid}")
-async def getProductById(itemid):
-    return {
-        "item" : itemid
-    }
+# @app.get("/items/{itemid}")
+# async def getProductById(itemid):
+#     return {
+#         "item" : itemid
+#     }
