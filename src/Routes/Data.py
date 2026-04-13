fafa36499f3b9550e7090f8dbf8fc3fa31c9bb55
pc <pc@DESKTOP-2N68CP9>
2026-04-13 10:11:59 +0200

    Handling Exception

--- src/Routes/Data.py
+++ src/Routes/Data.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends, UploadFile, HTTPException, status
 from fastapi.responses import JSONResponse
 from src.Helpers.config import get_settings, Settings
 from src.Controllers import DataController
-from src.Controllers  import ProjectController 
+from src.Controllers import ProjectController
+from src.Models.enums import responseEnum 
 import os
 import aiofiles
 Controller = DataController()
@@ -18,19 +19,20 @@ async def Upload(Project_id: str, file: UploadFile,
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": signal})
     # if valid then save the file
 
-    #using the PRoject Contoller function
-    
+    # using the PRoject Contoller function
+
     project = ProjectController()
 
     project_dir_path = project.getProjectPath(projectId=Project_id)
 
-    file_path = Controller.GenrateRandomFileName(file.filename , Project_id)
-    #write it binary 
-    async with aiofiles.open(file_path , "wb") as f:
-        #we gonna move chunk default size 
-        while chunk:= await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
-            await f.write(chunk)
+    file_path = Controller.GenrateRandomFileName(file.filename, Project_id)
+    # write it binary
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            # we gonna move chunk default size
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as ex:
+        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": responseEnum.ResponseSignal.FILE_UPLOAD_FAILED.value, "details": str(ex)})
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content={"message": signal})
-    
-
